1c066bcd5a16340f51e72e4ee87e4ec4080fb85f
Chirag Solanki <solankichiragcs12@gmail.com>
2026-01-01 11:04:07 +0530

    add read author category by query and post method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -24,3 +24,31 @@ async def read_book(book_title: str):
   for book in BOOKS:
      if book.get('title').casefold() == book_title.casefold():  
         return book
+
+@app.get('/books/')
+async def read_category_by_query(category:str):
+   books_to_return = []
+   for book in BOOKS:
+      if book.get('category').casefold() == category.casefold():
+         books_to_return.append(book)
+   return books_to_return
+   
+@app.get('/books/{book_author}/')
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if (book.get('author').casefold() == book_author.casefold() and
+            book.get('category').casefold() == category.casefold()):  # type: ignore
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+   BOOKS.append(new_book)
+
+@app.put('/books/update_book')
+async def update_book(updated_book = Body()):
+   for i in range(len(BOOKS)):
+      if BOOKS[i].get('title').casefold() == update_book.get('title').casefold():
+         BOOKS[i] = update_book
